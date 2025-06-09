704536af729c23de7f86f83b1c8337d383814666
Kirill <kir.cher123789@gmail.com>
2025-06-09 23:55:29 +0300

    da

--- Magazine.WebApi/Program.cs
+++ Magazine.WebApi/Program.cs
@@ -37,11 +37,6 @@ builder.Services.AddCors(options =>
 //builder.Services.AddSingleton<IProductService, ProductService>();
 
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
-    db.Database.Migrate();
-}
 //if (app.Environment.IsDevelopment())
 //{
 app.UseSwagger();
